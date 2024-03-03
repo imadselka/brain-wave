db36b392042bd4b311186539505a0d26cf91a635
Imad Selka <imadselkawi133@gmail.com>
2024-03-03 22:54:38 +0100

    Add Header component to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import ButtonGradient from "./assets/svg/ButtonGradient";
+import Header from "./components/Header";
 
 const App = () => {
   return (
     <>
-      <h1>React App</h1>
+      <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
+        <Header />
+      </div>
       <ButtonGradient></ButtonGradient>
     </>
   );
