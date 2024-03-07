d47ddfa01b706531699c34b4a2a2640fbd913536
Imad Selka <imadselkawi133@gmail.com>
2024-03-07 14:24:15 +0100

    Refactor Benefits component to use constants and add new section

--- src/components/Benifits.jsx
+++ src/components/Benifits.jsx
@@ -1,7 +1,32 @@
-import React from "react";
-
+import { benefits } from "../constants";
+import Heading from "./Heading";
+import Section from "./Section";
 const Benifits = () => {
-  return <div>Benifits</div>;
+  return (
+    <Section id="features">
+      <div className="container relative z-2">
+        <Heading
+          className="md:max-w-md lg:max-w-2xl"
+          title="Chat Smarter, Not Harder with Brainwave"
+        />
+        <div className="flex flex-wrap gap-10 mb-10">
+          {benefits.map((item) => (
+            <div
+              className="block relative p-0.5 bg-no-repeat bg-[length:100%_100%] md:max-w-[24rem]"
+              style={{
+                backgroundImage: `url(${item.backgroundUrl})`,
+              }}
+              key={item.id}
+            >
+              <div>
+                <h5>{item.title}</h5>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </Section>
+  );
 };
 
 export default Benifits;
