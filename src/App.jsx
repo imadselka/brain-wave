07845d6088a668dc5dc91867bf9a6ba7510156f7
Imad Selka <imadselkawi133@gmail.com>
2024-03-07 14:24:22 +0100

    Fix import typo in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import ButtonGradient from "./assets/svg/ButtonGradient";
-import Benifits from "./components/Benifits";
+import Benefits from "./components/Benefits";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
 
@@ -9,7 +9,7 @@ const App = () => {
       <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
         <Header />
         <Hero />
-        <Benifits />
+        <Benefits />
       </div>
       <ButtonGradient></ButtonGradient>
     </>
