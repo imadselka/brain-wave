379180324a1831aef7bec7bb6aa87732ec85eeb1
Imad Selka <imadselkawi133@gmail.com>
2024-03-08 23:50:25 +0100

    Refactor Benefits component styles

--- src/components/Benefits.jsx
+++ src/components/Benefits.jsx
@@ -8,7 +8,7 @@ import { GradientLight } from "./design/Benefits";
 const Benefits = () => {
   return (
     <Section id="features">
-      <div className="container relative z-2">
+      <div className="container relative z-2 ">
         <Heading
           className="md:max-w-md lg:max-w-2xl"
           title="Chat Smarter, Not Harder with Brainwave"
@@ -17,10 +17,8 @@ const Benefits = () => {
         <div className="flex flex-wrap gap-5 mb-10">
           {benefits.map((item) => (
             <div
-              className="block relative p-0.5 bg-no-repeat bg-[length:100%_100%] md:max-w-[24rem]"
-              style={{
-                backgroundImage: `url(${item.backgroundUrl})`,
-              }}
+              className="block relative p-0.5 bg-no-repeat bg-[length:100%_100%] md:max-w-[24rem] hover:bg-[length:100%_100%]"
+              style={{ backgroundImage: `url(${item.backgroundUrl})` }}
               key={item.id}
             >
               <div className="relative z-2 flex flex-col min-h-[22rem] p-[2.4rem] pointer-events-none">
@@ -35,11 +33,11 @@ const Benefits = () => {
                   />
                   <a
                     href={item.href}
-                    className="ml-auto font-code text-xs font-bold text-n-1 uppercase tracking-wider"
+                    className="flex ml-auto font-code text-xs font-bold text-n-1 uppercase tracking-wider"
                   >
                     Explore more
+                    <Arrow />
                   </a>
-                  <Arrow />
                 </div>
               </div>
 
